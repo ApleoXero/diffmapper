960181b3cfe20df7cce7bdcb2101bbf53e7af317
ApijaySh <apijaysharma@gmail.com>
2025-05-07 00:55:46 +0530

    Added Models and IO

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from diffmapper!")
+from modules import difmapper
 
+
+def main():
+    difmapper.DiffMapper(r"E:\Apijay@Projects\ProjectOpenTools\data\images\sample.png", r"E:\Apijay@Projects\ProjectOpenTools\data\images\sample.png", r"E:\Apijay@Projects\ProjectOpenTools\data\exports")
 
 if __name__ == "__main__":
     main()
